2926a1b7b15982818d5f448a0903d0b56d45dd4c
Anmol-M-0 <amaniyar000@gmail.com>
2021-05-29 19:47:05 +0530

    update

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# A simple CLI app for my practice 😋
+# Simple CLI apps for my practice 😋
+
+## app/cliapp/customerCredit/main.go 
+    contains a simple cli app that creates customer with credit limits and can list, add customer, and add to customer's credit limit
---
+++ app/cliapp/customerCredit/main.go
@@ -0,0 +1,149 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/Anmol-M-0/goSimpleCLIapp/app/cliapp/customerCredit/hello"
+	cust "github.com/Anmol-M-0/goSimpleCLIapp/business/customer"
+)
+
+var sc *bufio.Scanner = bufio.NewScanner(os.Stdin)
+
+func main() {
+	RunCustomerCreditApp()
+}
+
+func RunCustomerCreditApp() {
+	fmt.Println("hello world")
+	fmt.Println(hello.Hello())
+	fmt.Println("enter number of input")
+	var size int = scanInt()
+	// fmt.Scanf("%d", &size)
+	// sc.Scan()
+
+	var customerList []cust.Customer = make([]cust.Customer, size)
+	var tmpCustomer cust.Customer
+
+	var err error
+
+	for i := range customerList {
+		tmpCustomer, err = createCustomer()
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		if err = findByEmail(customerList, tmpCustomer.Email); err != nil {
+			fmt.Println(err)
+			continue
+		}
+		customerList[i] = tmpCustomer
+	}
+	for _, v := range customerList {
+		fmt.Println(v)
+	}
+	var command string
+	var tmpString string
+loop:
+	for {
+		fmt.Println("please enter case to execute, q to quit")
+		command = scanString()
+		switch command {
+		case "stop", "1000", "exit", "q":
+			break loop
+		case "1", "new", "new customer":
+			tmpCustomer, err = createCustomer()
+			if err != nil {
+				continue
+			}
+			if err = findByEmail(customerList, tmpCustomer.Email); err != nil {
+				fmt.Println(err)
+				continue
+			}
+			customerList = append(customerList, tmpCustomer)
+		case "2", "show", "show all":
+			for _, v := range customerList {
+				fmt.Println(v)
+			}
+		case "change credit limit", "3":
+			var index int
+			tmpString = scanString()
+			tmpCustomer, index, err = findByName(customerList, tmpString)
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+			fmt.Println("changing the credit limit of customer", tmpCustomer.Name)
+			fmt.Println("enter 'add' to add to credit limit, 'rem' to subtract from the same")
+			tmpString = scanString()
+			switch tmpString {
+			case "add":
+				fmt.Println("enter the amount to add to the credit limit of", tmpCustomer.Name)
+				customerList[index].CreditLimit = customerList[index].CreditLimit + scanFloat()
+			case "rem":
+				fmt.Println("enter the amount to remove from the credit limit of", tmpCustomer.Name)
+				customerList[index].CreditLimit = customerList[index].CreditLimit - scanFloat()
+			default:
+				fmt.Println("invalid option")
+			}
+		}
+
+	}
+}
+
+//create a customer instance by reading values from std input (console),
+//return empty customer, error if invalid input
+func createCustomer() (c cust.Customer, err error) {
+	var name, email string
+	var age int
+	var creditLimit float64
+	fmt.Println("<name> <email> <age> <creditLimit>")
+	fmt.Scanf("%s %s %d %f", &name, &email, &age, &creditLimit)
+	sc.Scan()
+	c, err = hello.NewCustomer(name, email, age, creditLimit)
+	return
+}
+
+//scan for a float64 from std input (console)
+func scanFloat() (f float64) {
+	fmt.Scanf("%f", &f)
+	sc.Scan()
+	return
+}
+
+//scan for a int from std input (console)
+func scanInt() (i int) {
+	fmt.Scanf("%d", &i)
+	sc.Scan()
+	return
+}
+
+//scan for a string from std input (console)
+func scanString() (str string) {
+	sc.Scan()
+	str = sc.Text()
+	str = strings.TrimSpace(str)
+	return
+}
+
+//from <customerList> find the customer and it's index, return (empty customer, -1, error) otherwise
+func findByName(customerList []cust.Customer, name string) (c cust.Customer, index int, err error) {
+	for i, v := range customerList {
+		if v.Name == name {
+			return v, i, nil
+		}
+	}
+	return cust.Customer{}, -1, errors.New("customer not found")
+}
+
+func findByEmail(customerList []cust.Customer, email string) error {
+	for _, v := range customerList {
+		if v.Email == email {
+			return errors.New("this email is already present")
+		}
+	}
+	return nil
+}
